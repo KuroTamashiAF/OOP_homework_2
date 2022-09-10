ee009608d42805757dcb9ba2fd0a1a88924a4a35
Alex <chugun777akk@mail.ru>
2022-09-10 15:11:27 +0700

    add relation

---
+++ RalationHumanAnimal.java
@@ -0,0 +1,8 @@
+public class RalationHumanAnimal {
+    private Human human;
+    private Animal animal;
+    public RalationHumanAnimal(Human huma,Animal anima ) {
+        this.human = human;
+        this.animal = anima;
+    }
+}
