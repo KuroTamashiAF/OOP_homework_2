7e6dcee64150f53334a3a8b7fe6efcff471d5caf
Alex <chugun777akk@mail.ru>
2022-09-11 15:16:46 +0700

    Add interaction

--- Action.java
+++ Action.java
@@ -10,8 +10,27 @@ public class Action extends RalationHumanAnimal {
             System.out.printf("%s call all Animals to him", human.GetName());
             // for (iterable_type iterable_element : iterable) {
                 System.out.printf("%s come to the kitchen ", humanToAinmals.get(human));
-
         }
     }
+    public void PetAnCat(Human human, Class<?> cls){
+        for (Animal animal : humanToAinmals.get(human)) {
+            if (cls.isInstance(animal) && animal.getClass()==Cat.class){
+                System.out.printf("%s is happy",animal.getNickname() );
+            }
+            System.out.println();
+            if (cls.isInstance(animal) && animal.getClass()==Dog.class){
+                System.out.printf("%s is happy",animal.getNickname() );
+            }
+            
+        }
+
+    }
+    /*public void PetAnDog(Human human, Class<?> cls){
+        for (Animal animal : humanToAinmals.get(human)) {
+            if (cls.isInstance(animal) && animal.getClass()==Dog.class){
+                System.out.printf("%s is happy",animal.getNickname() );
+            }
+            
+        }*/
 
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         Human hunan1 = new Human("Alex");
         Animal animalCat = new Cat("Murka");
         Animal animalDog = new Dog("Barlog");
+        
         // Animal AnimalCat1 = new Cat ("Barsicik");
         RealayionInterface relations = new RalationHumanAnimal();
         // relations.addAnimaltoHuman(hunan1, animalCat);
@@ -13,7 +14,8 @@ public class Main {
         Action action1 =  new Action();
         action1.addAnimaltoHuman(hunan1, animalCat);
         action1.addAnimaltoHuman(hunan1, animalDog);
-        action1.SummoningTechnique(hunan1);
-        
+        //action1.SummoningTechnique(hunan1);
+        System.out.println();
+        action1.PetAnCat(hunan1, Animal.class);
     }
 }
